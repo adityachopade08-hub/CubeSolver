fc517db96f98faa029005f86d14a80cb7b3cc7e6
Aditya <adityachopade08@gmail.com>
2026-07-01 23:45:05 +0530

    Stable Engine v1 - All six faces moves working

--- src/engine/math/RotationEngine.js
+++ src/engine/math/RotationEngine.js
@@ -24,6 +24,9 @@ class RotationEngine {
                         belongs = p.z === layer;
                         break;
 
+                    default:
+                        return;
+
                 }
 
                 if (!belongs)
@@ -35,8 +38,7 @@ class RotationEngine {
                     clockwise
                 );
 
-                RotationEngine.rotateStickers(
-                    cubelet,
+                cubelet.rotate(
                     axis,
                     clockwise
                 );
@@ -55,38 +57,101 @@ class RotationEngine {
 
             case "x":
 
-                if (clockwise)
-                    cubelet.position = { x, y: -z, z: y };
-                else
-                    cubelet.position = { x, y: z, z: -y };
+                if (clockwise) {
+
+                    cubelet.position = {
+
+                        x,
+
+                        y: z,
+
+                        z: -y
+
+                    };
+
+                }
+
+                else {
+
+                    cubelet.position = {
+
+                        x,
+
+                        y: -z,
+
+                        z: y
+
+                    };
+
+                }
 
                 break;
 
             case "y":
 
-                if (clockwise)
-                    cubelet.position = { x: z, y, z: -x };
-                else
-                    cubelet.position = { x: -z, y, z: x };
+                if (clockwise) {
+
+                    cubelet.position = {
+
+                        x: z,
+
+                        y,
+
+                        z: -x
+
+                    };
+
+                }
+
+                else {
+
+                    cubelet.position = {
+
+                        x: -z,
+
+                        y,
+
+                        z: x
+
+                    };
+
+                }
 
                 break;
 
             case "z":
 
-                if (clockwise)
-                    cubelet.position = { x: -y, y: x, z };
-                else
-                    cubelet.position = { x: y, y: -x, z };
+                if (clockwise) {
+
+                    cubelet.position = {
+
+                        x: -y,
+
+                        y: x,
+
+                        z
+
+                    };
+
+                }
+
+                else {
+
+                    cubelet.position = {
+
+                        x: y,
+
+                        y: -x,
+
+                        z
+
+                    };
+
+                }
 
                 break;
 
         }
-
-    }
-
-    static rotateStickers(cubelet, axis, clockwise) {
-
-        cubelet.rotate(axis, clockwise);
 
     }
 
